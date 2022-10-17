198fe392a7cf8e4a5015b97b240fce2f88d8cee4
NikaKekelidze-sys <nika.kekelidze94@gmail.com>
2022-10-17 16:08:23 +0400

    asd
    asd

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+const divroot = document.getElementById('root')
+const navbar = (
+    <nav>
+        <h1>Brand Name</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, divroot)
